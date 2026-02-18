868860057cb224e646346ad3fdc1488844ddc047
deoguah <43589132+deoguah@users.noreply.github.com>
2026-02-18 14:51:06 -0600

    Update 01_build_modeling_table.sql

--- sql/01_build_modeling_table.sql
+++ sql/01_build_modeling_table.sql
@@ -1,10 +1,39 @@
 -- SQL to create modeling table from raw churn data
-CREATE TABLE telco_model AS
-SELECT customerID,
-       gender,
-       SeniorCitizen,
-       tenure,
-       MonthlyCharges,
-       TotalCharges,
-       Churn
+DROP TABLE IF EXISTS telco_modeling;
+
+CREATE TABLE telco_modeling AS
+SELECT
+  customerID,
+  churn,
+  tenure,
+  MonthlyCharges,
+  TotalCharges,
+
+  -- Create a bucketed tenure category (easier to interpret than raw tenure)
+  CASE
+    WHEN tenure IS NULL THEN NULL
+    WHEN tenure < 6 THEN '0-5'
+    WHEN tenure < 12 THEN '6-11'
+    WHEN tenure < 24 THEN '12-23'
+    WHEN tenure < 48 THEN '24-47'
+    ELSE '48+'
+  END AS tenure_bucket,
+
+  -- Categorical fields used by the model
+  gender,
+  SeniorCitizen,
+  Partner,
+  Dependents,
+  PhoneService,
+  MultipleLines,
+  InternetService,
+  OnlineSecurity,
+  OnlineBackup,
+  DeviceProtection,
+  TechSupport,
+  StreamingTV,
+  StreamingMovies,
+  Contract,
+  PaperlessBilling,
+  PaymentMethod
 FROM telco_raw;
